83157c16287737193fbebc150d95ad8bb6ff515d
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-04-17 23:05:11 +0300

    new

---
+++ assets/js/index.js
@@ -0,0 +1,134 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const header = document.querySelector('header');
+const menuBg = document.querySelector('.menu-bg');
+const headerBody = document.querySelector('.header-body');
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    burger.addEventListener("click", function (e) {
+        if (menu.classList.contains('active')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+    menuBg.addEventListener("click", function (e) {
+        if (menu.classList.contains('active')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+    document.querySelectorAll('.menu-list li a').forEach(link => {
+        link.addEventListener("click", closeMenu);
+    });
+
+    function openMenu() {
+        menu.classList.add('active');
+        burger.classList.add('active');
+        header.classList.add('active');
+        menuBg.classList.add('active');
+        headerBody.classList.add('active');
+
+
+    }
+
+    function closeMenu() {
+        menu.classList.remove('active');
+        burger.classList.remove('active');
+        header.classList.remove('active');
+        menuBg.classList.remove('active');
+        headerBody.classList.remove('active');
+
+    }
+
+
+});
+
+window.addEventListener('scroll', function () {
+    const headerTop = document.querySelector('header')
+
+
+    if (window.scrollY > 0) {
+        headerTop.classList.add('moved');
+
+    } else {
+        headerTop.classList.remove('moved');
+
+    }
+});
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    question.addEventListener('click', () => {
+        item.classList.toggle('active');
+
+        // закрывает остальные
+        faqItems.forEach(otherItem => {
+            if (otherItem !== item) {
+                otherItem.classList.remove('active');
+            }
+        });
+    });
+});
+
+document.querySelectorAll('.home-link ').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const headerHeight = document.querySelector('.header').offsetHeight;
+            const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (!localStorage.getItem("cookiesAccepted")) {
+        const cookiesModal = document.getElementById("cookies-modal");
+        cookiesModal.style.display = "flex";
+
+        document.getElementById("accept-cookies").addEventListener("click", function() {
+            localStorage.setItem("cookiesAccepted", "true");
+
+            cookiesModal.style.display = "none";
+        });
+    }
+});
+
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+
